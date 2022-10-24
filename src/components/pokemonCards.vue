7ba096dd54982b7734f66fc3843a2a3fe79db176
M Ridwan Riansyah <67872022+ryansyrl@users.noreply.github.com>
2022-10-24 22:59:27 +0700

    feat(fetch pokemon on load): fetch default pokemon on load

---
+++ src/components/pokemonCards.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="shadow-sm rounded-lg border border-solid border-gray-200 p-5 w-[270px]">
+    <p>123</p>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,16 +2,27 @@
 import axios from 'axios'
 import { inject, onMounted, ref } from 'vue'
 import Searchbar from '../components/searchBar.vue'
+import PokemonCard from '../components/pokemonCards.vue'
 const $pokeDex = inject('$pokeDex')
+const pokemonList = ref([])
 onMounted(async () => {
-  await axios.get(`${$pokeDex}pokemon`)
+  const fetched = await axios.get(`${$pokeDex}pokemon?limit=21`)
+  if (fetched.status === 200)
+    pokemonList.value = fetched.data.results
 })
 </script>
 
 <template>
-  <div class="container pt-10">
+  <div class="container pt-10 mx-auto">
     <div class="mx-auto max-w-7xl">
       <Searchbar />
+    </div>
+
+    <div class="mt-6 flex max-w-7xl mx-auto">
+      <div class="w-2/3 flex items-center justify-between gap-y-10 flex-wrap">
+        <!-- <h1>123</h1> -->
+        <PokemonCard v-for="item in pokemonList" :key="item.idx" />
+      </div>
     </div>
   </div>
 </template>
