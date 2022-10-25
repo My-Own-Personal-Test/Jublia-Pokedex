52343b1e2d7edea7119f773d13f9623cc08b6173
M Ridwan Riansyah <67872022+ryansyrl@users.noreply.github.com>
2022-10-25 10:51:33 +0700

    feat(infinite scroll): add infinite scroll

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import axios from 'axios'
 import { defineComponent, inject, onMounted, ref } from 'vue'
+import { useInfiniteScroll } from '@vueuse/core'
 import Searchbar from '../components/searchBar.vue'
 import PokemonCard from '../components/pokemonCards.vue'
 import SkeletonCards from '../components/skeletonsCard.vue'
+
 defineComponent({
   components: {
     Searchbar,
@@ -11,11 +13,14 @@ defineComponent({
     SkeletonCards,
   },
 })
+
 const $pokeDex = inject('$pokeDex')
 const loading = ref(true)
 const pokemonList = ref([])
-onMounted(async () => {
-  const fetched = await axios.get(`${$pokeDex}pokemon?limit=21`)
+const el = ref(null)
+
+const getPokemons = async (payload) => {
+  const fetched = await axios.get(`${$pokeDex}pokemon?limit=21&offset=${payload ? pokemonList.value.length : 0}`)
   if (fetched.status === 200) {
     const arr = fetched.data.results
     for (let i = 0; i < arr.length; i++) {
@@ -24,6 +29,18 @@ onMounted(async () => {
       loading.value = false
     }
   }
+}
+
+useInfiniteScroll(
+  el,
+  () => {
+    getPokemons(true)
+  },
+  { distance: 21 },
+)
+
+onMounted(() => {
+  getPokemons()
 })
 </script>
 
@@ -34,7 +51,7 @@ onMounted(async () => {
     </div>
 
     <div class="mt-6 flex max-w-7xl mx-auto">
-      <div v-if="!loading" class="w-2/3 flex items-center justify-between gap-y-10 flex-wrap">
+      <div v-if="!loading" ref="el" class="w-2/3 overflow-y-auto max-h-screen flex items-center justify-between gap-y-10 flex-wrap">
         <PokemonCard v-for="item in pokemonList" :key="item.idx" :name="item.name" :img="item.sprites.front_default" :types="item.types" />
       </div>
       <div v-else class="w-2/3 flex items-center justify-between gap-y-10 flex-wrap">
