b86c30d7000cea722fe9fc698f0490c075569872
M Ridwan Riansyah <67872022+ryansyrl@users.noreply.github.com>
2022-10-26 23:35:05 +0700

    fix(filter): fix double url on request

--- src/composables/filterPokemon.js
+++ src/composables/filterPokemon.js
@@ -11,7 +11,7 @@ const useFilterPokemon = () => {
       getPokemons()
       return
     }
-    const filtered = await axiosInstance.get(`${import.meta.env.VITE_BASE_URL}type/${payload}`)
+    const filtered = await axiosInstance.get(`type/${payload}`)
     const arr = filtered.data.pokemon
     pokemonList.value = []
     for (let i = 0; i < arr.length; i++) {
